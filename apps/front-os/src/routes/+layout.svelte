5b92a2c17fae570aa13bfd136faeaaa53ee3b94c
Ian <ianlaan@outlook.com>
2023-07-17 00:48:27 +0200

    added vercel analytics

--- apps/front-os/src/routes/+layout.svelte
+++ apps/front-os/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 import "../app.css";
+import { inject } from '@vercel/analytics';
+import { dev } from '$app/environment';
 
-
+inject({ mode: dev ? 'development' : 'production' })
 </script>
 
 <slot />
